f9c404d8c78244b336937c21132ba62179311e10
j50n <j50n@lilypepper.com>
2021-01-02 22:49:50 -0700

    extra rnd call to mask first number

--- src/utility/math.f
+++ src/utility/math.f
@@ -1,11 +1,13 @@
-variable (rnd)
-utime drop #7 * #1073741825 + (rnd) !
+variable (rnd-seed)
+utime drop #7 * #1073741825 + (rnd-seed) !
 
 \ https://en.wikipedia.org/wiki/Xorshift
 \ Pseudo-random number generator adapted from the 64-bit C shift-register generator.
 : rnd  ( -- n )
-    (rnd) @
+    (rnd-seed) @
     dup 13 lshift xor 
     dup 7 rshift xor 
     dup 17 lshift xor 
-    dup (rnd) ! ;
+    dup (rnd-seed) ! ;
+
+rnd
